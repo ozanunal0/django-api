3f0c7152ad20314f2fbf73d4c60dc15327c43d83
KendineYazilimci <ozanunal@protonmail.com>
2022-04-15 00:25:59 +0300

    Django API Final

--- api/models.py
+++ api/models.py
@@ -5,7 +5,6 @@ class Note(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
 def __str__(self):
     return '%s %s'%(self.title, self.body)
